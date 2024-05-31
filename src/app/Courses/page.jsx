5e9aa47804fb0bff513e3720999b4e942a4d9fed
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-05-31 21:33:44 +0530

    Box Error fixed

--- src/app/Courses/page.jsx
+++ src/app/Courses/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react'
 export default function Courses() {
   return (
     <div className='h-[100vh]  flex bg-gradient-to-r bg-clip-text from-purple-700 via-indigo-300 to-blue-700 justify-center'>
